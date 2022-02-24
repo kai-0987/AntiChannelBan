dab34ad41883c35550dc7411fae474999239aab4
Kai <90447061+kai-0987@users.noreply.github.com>
2022-02-24 22:30:06 +0530

    Update cmd_start.py

--- ChannelBan/Plugins/cmd_start.py
+++ ChannelBan/Plugins/cmd_start.py
@@ -4,21 +4,18 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Client.on_message(filters.command(["start", "start@ChannelBanRobot"]))
 async def start(_, message: Message):
     await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
- `Heya I'm A Anti Channel Tegram bot to delete and ban message sent by channel`""",
+        f"""<b>Hi {message.from_user.first_name}!</b>
+ `Heya I'm A Anti-Channel telegram bot to delete and ban message sent by channels in groups`""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "📢 Cʜᴀɴɴᴇʟ", url="https://t.me/DeeCodeBots"
-                    ),
-                    InlineKeyboardButton(
-                        "Sᴜᴘᴘᴏʀᴛ 👥", url="https://t.me/DeCodeSupport"
+                        "Group", url="https://t.me/international_english_chattings"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "🧑‍💻 Dᴇᴠ 🧑‍💻", url="https://t.me/DeeCodeDevs"
+                        "IFCC Admin", url="https://t.me/IFCR_BOT"
                     )
                 ]
             ]
